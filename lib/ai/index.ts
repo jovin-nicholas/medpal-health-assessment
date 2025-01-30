49638faa90d4bb28aeee8fcdd28bab05433b9fd7
Jovin Nicholas <32580407+jovin-nicholas@users.noreply.github.com>
2025-01-29 21:06:47 -0500

    add gemini support

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,11 +1,15 @@
 import { openai } from '@ai-sdk/openai';
+import { google } from '@ai-sdk/google';
+// import { groq } from '@ai-sdk/groq';
+
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
-    model: openai(apiIdentifier),
+    // model: openai(apiIdentifier),
+    model: google(apiIdentifier),
     middleware: customMiddleware,
   });
 };
--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -9,6 +9,12 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
+    id: 'gemini-1.5-flash',
+    label: 'Gemini 1.5 Flash',
+    apiIdentifier: 'gemini-1.5-flash',
+    description: 'For short, snappy responses',
+  },
+  {
     id: 'gpt-4o-mini',
     label: 'GPT 4o mini',
     apiIdentifier: 'gpt-4o-mini',
@@ -22,4 +28,4 @@ export const models: Array<Model> = [
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
+export const DEFAULT_MODEL_NAME: string = 'gemini-1.5-flash';
